37e701fa7a362aa0e0c70142f4e390cf407f8413
Albert <ayang04@zohomail.com>
2022-09-29 21:14:35 -0700

    1

--- A62.hpp
+++ A62.hpp
@@ -1,19 +1,61 @@
 //
-
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
-// complete two functions
-double nthpowerp(int n)
+double nthpowerp(int);
+double nthpowern(int);
+void printout(int);
+
+int main ()
 {
-        // return n_th power number
+
+  int input,result;
+
+  cout << "Enter your input" << endl;
+  cin >> input;
+
+  if (input > 0)
+  {
+    result = nthpowerp(input);
+  }
+
+  else if (input < 0)
+  {
+    result = nthpowern(input);
+  }
+  else
+  {
+    result = 1;
+  }
+
+  printout(result);
+
+
 }
-double nthpowern(int n)
+
+double nthpowerp(int power)
 {
-        // return n_th power number
+  int result = 1;
+  for(int i = 0; i < power; ++i)
+  {
+    result *= 2;
+  }
+  return result;
 }
-void printout(double result)
+
+double nthpowern(int power)
 {
-        cout << "The result is " << setw(10) << fixed << setprecision(5) << result << endl;
+  int result = 1;
+  for(int i = 0; i < power; ++i)
+  {
+    result *= 0.5;
+  }
+  return result;
+}
+
+void printout(int result)
+{
+  cout << result;
 }
