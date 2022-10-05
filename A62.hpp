93048892591efbcce25d36bfd8d87a7b652ad2c2
Albert Yang <70829217+Basalt49re@users.noreply.github.com>
2022-10-04 19:36:00 -0700

    Update and rename A62.cpp to A62.hpp

---
+++ A62.hpp
@@ -0,0 +1,27 @@
+double nthpowerp(int power)
+{
+  int result = 1;
+  for(int i = 0; i < power; ++i)
+  {
+    result *= 2;
+  }
+  return result;
+}
+
+double nthpowern(int n)
+{
+  double result = 1;
+  int power = n;
+  for(int i = 0; i < abs(power); ++i)
+  {
+    cout << power << endl;
+    result *= .5;
+  }
+  double invertedpower = 1.0/result;
+  return invertedpower;
+}
+
+void printout(int result)
+{
+  cout << result;
+}
